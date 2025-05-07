9f3133ea2cf1d45163f1cf3d8ff858e59b778c60
erivas <erivas@gpconsultores.cl>
2025-05-07 16:51:39 -0400

    se agrega composer dump-autoload

---
+++ app/Helpers/contador.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Request;
+use App\Models\Visita;
+use App\Models\IpVisita;
+
+function contarVisita()
+{
+    $fechaHoy = date('Y-m-d');
+    $ip = Request::ip();
+
+    // Registrar visita total
+    $visita = Visita::firstOrNew(['fecha' => $fechaHoy]);
+    $visita->total = ($visita->total ?? 0) + 1;
+    $visita->save();
+
+    // Registrar IP si no existe
+    IpVisita::firstOrCreate([
+        'fecha' => $fechaHoy,
+        'ip' => $ip,
+    ]);
+}
--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,21 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EstacionPublicaController;
 use App\Http\Controllers\SectorController;
 
-/*Route::get('/', function () {
-    return view('mapa');
-});*/
+use App\Models\Visita;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\IpVisita;
 
-Route::get('/', function () {
+
+Route::get('/', function ()
+{
+    contarVisita();
     return redirect('/sector/');
 });
 
-Route::get('/estacion-publica/{id_estacion}', function ($id_estacion) {
+Route::get('/estacion-publica/{id_estacion}', function ($id_estacion)
+{
+    contarVisita();
     return view('estacion-publica', ['id_estacion' => $id_estacion]);
 });
 
@@ -23,7 +29,9 @@ Route::get('/estacion-publica/{id_estacion}', [EstacionPublicaController::class,
 
 Route::get('/glosary', [SectorController::class, 'glosary']);
 
+
 Route::get('/sector/{id}', function ($id) {
+    contarVisita();
     return view('mapa', ['sector' => $id]);
 })->where('id', '[0-9]+'); 
 
