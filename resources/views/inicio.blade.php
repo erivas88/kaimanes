f787c479fb781eca2ee8fb695e4f65f9a5ca21a9
erivas <erivas@gpconsultores.cl>
2025-05-26 16:32:09 -0400

    delete blank #

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -28,7 +28,7 @@
                         Con el objetivo de mantener informada a la comunidad, <strong>Minera Los Pelambres</strong> implementó dos sitios web en los que los vecinos pueden encontrar información en línea respecto a agua y aire en la localidad.
                      </p>
                      <br>
-                     <a href="{{ url('/mapa') }}" class="custom-button-green"  >
+                     <a href="#" class="custom-button-green"  >
                         <i class="fas fa-wind icono-viento"></i>
                         <span class="texto-boton">
                         Ir al Sistema de Monitoreo de <br><strong> Calidad del Aire</strong>
@@ -36,7 +36,7 @@
                      </a>
                      <br>
                      
-                        <a href="{{ url('/mapa') }}" class="custom-button-blue" target="_blank">
+                        <a href="{{ url('/mapa') }}" class="custom-button-blue" target="">
                         <i class="fas fa-tint icono-viento"></i>
                         <span class="texto-boton">
                         Ir al Sistema de Monitoreo de <br><strong> Calidad del Agua</strong>
